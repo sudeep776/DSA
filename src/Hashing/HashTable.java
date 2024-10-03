9c18d4e0102ffebf245c2d6f60ca0d8c3c5a309d
Sudeep U <Sudeep7766@gmail.com>
2024-10-04 00:50:11 +0530

    hashmap

---
+++ src/Hashing/HashTable.java
@@ -0,0 +1,80 @@
+package Hashing;
+
+public class HashTable {
+    private HashNode[] buckets;
+    private int numOfBuckets;
+    private int size;
+    public HashTable(){
+        this(10);
+    }
+    public HashTable(int capacity){
+        this.numOfBuckets=capacity;
+        this.buckets=new HashNode[capacity];
+        this.size=0;
+    }
+    private class HashNode{
+        Integer key;
+        String value;
+        HashNode next;
+
+        public HashNode(int key,String value){
+            this.key=key;
+            this.value=value;
+        }
+    }
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public void put(Integer key,String value){
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        while(head!=null){
+            if(head.key.equals(key)){
+               head.value=value;
+               return;
+            }
+            head=head.next;
+        }
+        size++;
+        head=buckets[bucketIndex];
+        HashNode node = new HashNode(key,value);
+        node.next=head;
+        buckets[bucketIndex]=node;
+    }
+    public int getBucketIndex(Integer key){
+        return key%numOfBuckets;
+    }
+    public String get(Integer key){
+        int bucketIndex =  getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        while(head!=null){
+            if(head.key.equals(key)){
+                return head.value;
+            }
+            head=head.next;
+        }return null;
+    }
+
+    public String remove(Integer key){
+        Integer bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        HashNode previous = null;
+        while(head!=null){
+            if(head.key.equals(key)){
+                break;
+            }
+            previous=head;
+            head=head.next;
+        }
+        if(head==null) return null;
+        if(previous!=null){
+            previous.next=head.next;
+        }
+        else{
+            buckets[bucketIndex]=head.next;
+        }return head.value;
+    }
+}

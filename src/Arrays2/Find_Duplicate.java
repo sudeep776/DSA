832030f1b0acfe55f767864dbc4fc0f917f4c2e3
Sudeep U <Sudeep7766@gmail.com>
2024-11-05 00:07:57 +0530

    find_duplicate_added

--- src/Arrays2/Find_Duplicate.java
+++ src/Arrays2/Find_Duplicate.java
@@ -1,17 +1,30 @@
 package Arrays2;
 
-public class Find_Duplicate{
-    public static int findDuplicate(int arr[]){
-        int slow = arr[0];
-        int fast = arr[0];
-        while(true){
-            slow=arr[slow];
-            fast=arr[arr[fast]];
+import java.util.*;
 
+public class Find_Duplicate {
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Finding the intersection point in the cycle
+        do {
+            slow = nums[slow];          // Move slow pointer by 1 step
+            fast = nums[nums[fast]];    // Move fast pointer by 2 steps
+        } while (slow != fast);
+
+        // Phase 2: Finding the entrance to the cycle (duplicate number)
+        fast = nums[0];  // Start the fast pointer at the beginning
+        while (slow != fast) {
+            slow = nums[slow];  // Move both pointers by 1 step
+            fast = nums[fast];
         }
-    }
-    public static void main(String[] args) {
-        int arr[] = {3,1,3,4,2};
 
+        return slow; // Both pointers meet at the duplicate number
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 3, 4, 2, 3};
+        System.out.println("The duplicate element is " + findDuplicate(arr));
     }
 }
